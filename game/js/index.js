b4c87fb52089376f1b6b3eb6aef20de022d057db
Nutella Wilshere <czernn1@wis.edu.hk>
2015-11-13 08:56:02 +0800

    game added
    yay

---
+++ game/js/index.js
@@ -0,0 +1,33 @@
+function mygame() {
+alert("THE LEGEND OF THE PEPE")
+var username = prompt("Hi there! whats your name?");
+  confirm("Thats a weird name " + username + " ,Anyways lets start!")
+alert("There is chaos between the world of memes")
+alert("You are PePe the frog, A confused, derpy frog working with Boo the dog")
+alert("The world may seem happy but Doge and grumpy cat are after you. and you need to run.")
+alert("You goal is to kill all the Doges.")
+alert("However watch out for grumpy cat, if you touch him you loose")
+alert("GOOD LUCK PEPE!")
+
+alert("You hear boo yell out to you PEPE!RUNNN DOGE IS AFTER YOU!")
+
+ var situation = prompt("How do you solve the situation? Do you run or fight?")
+
+switch(situation){
+  case "run":
+    alert("YOU HAVE RUN INTO GRUMPY CAT");
+     var situation = prompt("How do you solve the situation? Do you run or fight?")
+     switch(situation){
+       case "run":
+         alert("You ran into doge. RIP")
+    alert("You have died. Sorry m8 you need to try harder next time");
+    break;
+       case "fight":
+         alert("You killed grumpy cat but doge snuck up on you and tried to kill you but you fought back")
+         alert("CONGRATULATIONS!YOU HAVE WON! HERES A COOKE :D")
+         
+        
+     }
+    
+alert("THANKS FOR PLAYING MY GAME!")}       
+}
